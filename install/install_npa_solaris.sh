85b3c185c4cdaee6d43d31942e213242ca877bfd
Chris Gilbert <chris.gilbert@corelogic.co.uk>
2010-08-16 12:36:06 +0100

    Committing svn removal

--- install/install_npa_solaris.sh
+++ install/install_npa_solaris.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-NPAUSER=npa
-NPAGROUP=npa
-INSTALLDIR=/usr/local/npa
-/usr/sbin/groupadd $NPAGROUP
-/usr/sbin/useradd $NPAUSER -g $NPAGROUP
+NPAUSER=oradev
+NPAGROUP=dbadev
+INSTALLDIR=/usr/local/corelogic/npa
+
+if [ $(who am i | awk '{print $1}') == 'root' ]; then
+  echo Root access is available, creating user and group.
+  groupadd $NPAGROUP
+  useradd -G $NPAGROUP $NPAUSER
+
 
 mkdir -p $INSTALLDIR/db
 
@@ -16,9 +20,9 @@ else
         echo Copying files to $INSTALLDIR produced errors!
 fi
 
-echo Changing $INSTALL_DIR/bin/npa file..
-sed "s/RUN_AS_USER=npa/RUN_AS_USER=$NPAUSER/" $INSTALL_DIR/bin/npa > $INSTALL_DIR/bin/npa.new
-mv $INSTALL_DIR/bin/npa.new $INSTALL_DIR/bin/npa
+echo Changing $INSTALLDIR/bin/npa file..
+sed "s/RUN_AS_USER=npa/RUN_AS_USER=$NPAUSER/" $INSTALLDIR/bin/npa > $INSTALLDIR/bin/npa.new
+mv $INSTALLDIR/bin/npa.new $INSTALLDIR/bin/npa
 chmod 755 $INSTALLDIR/bin/*
 
 chown -R $NPAUSER:$NPAGROUP $INSTALLDIR
@@ -30,15 +34,21 @@ else
 fi
 
 
+if [ $(who am i | awk '{print $1}') == 'root' ]; then
 ln -s $INSTALLDIR/bin/npa /etc/init.d/npa
 ln -s /etc/init.d/npa /etc/rc3.d/S99npa
 ln -s /etc/init.d/npa /etc/rc1.d/K01npa
 ln -s /etc/init.d/npa /etc/rc2.d/K01npa
 
 if [ $? -eq 0 ]; then
+
         echo Successfully installed npa service.
         exit 0
 else
         echo Chkconfig command to install npa service failed!
         exit 1
 fi
+else
+  echo No root access, service was not installed.
+fi
+
